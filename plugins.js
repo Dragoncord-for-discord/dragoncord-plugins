9bcc0da665da8171162c1486ef5b83d01a1935e0
DragonFire <64083584+DragonFire1230@users.noreply.github.com>
2022-03-29 16:12:48 +0500

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,3 +1,5 @@
+console.log("DragonCord Plugins");
+
 //import { * } from './plugins/discordexperiments.plugin.js';
 
 Object.defineProperty((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.isDeveloper!==void 0).exports.default,"isDeveloper",{get:()=>true});
