71d08906e6f5a01c0d1ae63c04f11a110db9d9a5
DragonFire <64083584+DragonFire1230@users.noreply.github.com>
2022-03-29 16:06:05 +0500

    Add files via upload

---
+++ plugins.js
@@ -0,0 +1,5 @@
+//import { * } from './plugins/discordexperiments.plugin.js';
+
+Object.defineProperty((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.isDeveloper!==void 0).exports.default,"isDeveloper",{get:()=>true});
+//const { testPlugin } = require('./plugins/testPlugin.js');
+//const { discordExperiments } = require('./plugins/discordexperiments.plugin.js');
